b4a8bd1286d180a0fe2939a549270806f67e5f5f
이슬아 <rheeseula@gmail.com>
2024-08-21 09:05:27 +0900

    feat: clear activity when closing tab

--- client/scripts/setActivity.js
+++ client/scripts/setActivity.js
@@ -1,18 +1,24 @@
-let nickname = document.getElementsByClassName("nickname")[0].innerText;
-let title = document.getElementsByClassName("broadcast_title")[0].innerText;
+let nickname = "";
+let title = "";
 let view = 0;
 let image = "";
+let url = "";
 
 function sendData() {
+  nickname = document.getElementsByClassName("nickname")[0].innerText;
+  title = document.getElementsByClassName("broadcast_title")[0].innerText;
   view = document.getElementById("nAllViewer").innerText;
+  image = document.getElementsByClassName("thum")[0].src;
+  url = window.location.href;
 
-  fetch("http://localhost:3000/", {
+  navigator.sendBeacon("http://localhost:3000/", {
     method: "POST",
     body: JSON.stringify({
       nickname: nickname,
       title: title,
       view: view,
       image: image,
+      url: url,
     }),
   });
 }
@@ -36,3 +42,7 @@ let getViewer = setInterval(() => {
     }, 30000);
   }
 }, 500);
+
+window.addEventListener("beforeunload", () => {
+  navigator.sendBeacon("http://localhost:3000/clear", JSON.stringify({}));
+});
