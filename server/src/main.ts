b2a3af329447018cfc006a2476003b0de3973c47
이슬아 <rheeseula@gmail.com>
2024-08-21 17:48:44 +0900

    refactor: server main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,17 +8,5 @@ const RPC = new RPCHandler();
 const Server = new HTTPServer(RPC);
 
 Server.start(Number(process.env.PORT) ?? 3000, () => {
-  try {
-    RPC.client.on("ready", async () => {
-      Server.setReady();
-    });
-  } catch (e) {
-    console.error(e);
-  }
-
-  RPC.client.login().catch((e) => {
-    console.log(e);
-  });
-
   console.log("Server is listening on port 3000");
 });
