9ccb50c5bd08cf5be94e3f006178317d1b365f4e
이슬아 <rheeseula@gmail.com>
2024-08-21 10:37:32 +0900

    feat: RPCInterface #clearActivity

--- server/src/rpc.ts
+++ server/src/rpc.ts
@@ -5,6 +5,7 @@ import ActivityBody from "./request";
 interface RPCInterface {
   client: Client;
   setActivity(activity: object): void;
+  clearActivity(): void;
 }
 
 class RPCHandler implements RPCInterface {
