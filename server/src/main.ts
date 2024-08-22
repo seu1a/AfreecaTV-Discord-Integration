bdcdcd64e56cad542224fa840cd7731557c30499
이슬아 <rheeseula@gmail.com>
2024-08-22 22:02:32 +0900

    feat: open tray on app ready

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,24 @@
+import { app } from "electron";
+import dotenv from "dotenv";
+
 import RPCHandler from "./rpc";
 import HTTPServer from "./server";
-import dotenv from "dotenv";
+import MenuBar from "./tray";
 
 dotenv.config();
 
 const RPC = new RPCHandler();
 const Server = new HTTPServer(RPC);
 
-Server.start(Number(process.env.PORT) ?? 3000, () => {
-  console.log("Server is listening on port 3000");
+const appElements: any = {
+  tray: null,
+  windows: [],
+};
+
+app.on("ready", () => {
+  appElements.tray = new MenuBar();
+
+  Server.start(process.env.PORT ? Number(process.env.PORT) : 3000, () => {
+    console.log(`server is ready`);
+  });
 });
